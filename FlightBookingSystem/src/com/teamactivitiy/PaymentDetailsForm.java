cf107218e66edf5d736ad7182b3cbe09d703c5f9
Brynt Dador <120770668+Bbrynt@users.noreply.github.com>
2023-12-21 01:05:20 +0800

    Seat Selection and Payment Details

--- FlightBookingSystem/src/com/teamactivitiy/PaymentDetailsForm.java
+++ FlightBookingSystem/src/com/teamactivitiy/PaymentDetailsForm.java
@@ -1,27 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Set;
 
 public class PaymentDetailsForm extends JDialog {
-    public PaymentDetailsForm(JFrame parent) {
+    private JTextField cardNumberField;
+    private JTextField expiryDateField;
+    private JTextField cvvField;
+    private Set<String> selectedSeats;
+    private JFrame mainAppFrame; // Reference to the main application frame
+
+    public PaymentDetailsForm(JFrame parent, Set<String> selectedSeats) {
         super(parent, "Payment Details", true);
-        setLayout(new FlowLayout());
-        setSize(300, 200);
+        this.mainAppFrame = parent; // Store the reference to the main application frame
+        this.selectedSeats = selectedSeats;
 
-        // Add components for payment information
-        // ...
+        // Layout setup
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        setSize(400, 200);
 
+        // Card number input
+        JPanel cardNumberPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        cardNumberPanel.add(new JLabel("Card Number:"));
+        cardNumberField = new JTextField(16);
+        cardNumberPanel.add(cardNumberField);
+        getContentPane().add(cardNumberPanel);
+
+        // Expiry date input
+        JPanel expiryDatePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        expiryDatePanel.add(new JLabel("Expiry Date (MM/YY):"));
+        expiryDateField = new JTextField(5);
+        expiryDatePanel.add(expiryDateField);
+        getContentPane().add(expiryDatePanel);
+
+        // CVV input
+        JPanel cvvPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        cvvPanel.add(new JLabel("CVV:"));
+        cvvField = new JTextField(3);
+        cvvPanel.add(cvvField);
+        getContentPane().add(cvvPanel);
+
+        // Book button
+        JPanel bookButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton bookButton = new JButton("Book");
-        bookButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new FinalizeBooking(parent).setVisible(true);
-            }
-        });
-        add(bookButton);
+        bookButton.setFont(new Font("Arial", Font.BOLD, 16)); // Set a larger font
+        bookButton.setPreferredSize(new Dimension(150, 50)); // Set the preferred size to make the button larger
+        bookButton.addActionListener(this::processPayment);
+        bookButtonPanel.add(bookButton);
+        getContentPane().add(bookButtonPanel);
 
         setLocationRelativeTo(parent);
+    }
+
+    private void processPayment(ActionEvent e) {
+        // Here you would handle the payment processing
+        // For now, just display a confirmation message
+        JOptionPane.showMessageDialog(this, "Payment successful!\nSeats: " + selectedSeats, "Booking Confirmed", JOptionPane.INFORMATION_MESSAGE);
+        closeAllWindowsExceptMain();
+    }
+
+    private void closeAllWindowsExceptMain() {
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window != mainAppFrame && window instanceof JDialog) {
+                window.dispose(); // Close the dialog
+            }
+        }
+        // Bring the main application frame to the front if needed
+        mainAppFrame.setState(JFrame.NORMAL);
+        mainAppFrame.toFront();
     }
 }
--- FlightBookingSystem/src/com/teamactivitiy/SeatSelectionForm.java
+++ FlightBookingSystem/src/com/teamactivitiy/SeatSelectionForm.java
@@ -1,27 +1,95 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SeatSelectionForm extends JDialog {
+    private static final int NUM_ROWS = 10;
+    private static final int NUM_COLUMNS_PER_SIDE = 2; // Number of columns per side of the aisle
+    private Set<String> selectedSeats = new HashSet<>();
+    private ImageIcon seatIcon;
+
     public SeatSelectionForm(JFrame parent) {
         super(parent, "Seat Selection", true);
-        setLayout(new FlowLayout());
-        setSize(300, 200);
+        getContentPane().setLayout(new BorderLayout()); // Use BorderLayout for the main layout
+        setSize(1000, 800);
 
-        // Add components for seat selection
-        // ...
-
-        JButton nextButton = new JButton("Next");
-        nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new PaymentDetailsForm(parent).setVisible(true);
-            }
-        });
-        add(nextButton);
+        loadSeatIcon();
+        createSeatButtons();
+        addNextButton();
 
         setLocationRelativeTo(parent);
+    }
+
+    private void loadSeatIcon() {
+        try {
+            ImageIcon originalIcon = new ImageIcon("C:\\Users\\Dador\\Documents\\GitHub\\Flight-Booking-System\\FlightBookingSystem\\src\\com\\teamactivitiy\\seat.png");
+            Image originalImage = originalIcon.getImage();
+            Image scaledImage = originalImage.getScaledInstance(30, 30, Image.SCALE_SMOOTH); // Scale it to 30x30
+            seatIcon = new ImageIcon(scaledImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Failed to load seat icon.", "Error", JOptionPane.ERROR_MESSAGE);
+            seatIcon = null;
+        }
+    }
+
+    private void createSeatButtons() {
+        JPanel leftPanel = new JPanel(new GridLayout(NUM_ROWS, NUM_COLUMNS_PER_SIDE));
+        JPanel rightPanel = new JPanel(new GridLayout(NUM_ROWS, NUM_COLUMNS_PER_SIDE));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int column = 0; column < NUM_COLUMNS_PER_SIDE; column++) {
+                leftPanel.add(createSeatButton(row, column));
+            }
+            for (int column = 0; column < NUM_COLUMNS_PER_SIDE; column++) {
+                rightPanel.add(createSeatButton(row, column + NUM_COLUMNS_PER_SIDE + 1)); // Offset for aisle
+            }
+        }
+
+        mainPanel.add(leftPanel);
+        mainPanel.add(Box.createHorizontalStrut(40)); // This represents the aisle
+        mainPanel.add(rightPanel);
+
+        getContentPane().add(mainPanel, BorderLayout.CENTER);
+    }
+
+    private JButton createSeatButton(int row, int seatPosition) {
+        char seatChar = (char) ('A' + seatPosition % (NUM_COLUMNS_PER_SIDE * 2));
+        String seatId = String.format("%d%c", row + 1, seatChar);
+        JButton seatButton = new JButton(seatIcon);
+        seatButton.setActionCommand(seatId);
+        seatButton.addActionListener(this::toggleSeatSelection);
+        return seatButton;
+    }
+
+    private void toggleSeatSelection(ActionEvent e) {
+        JButton seatButton = (JButton) e.getSource();
+        String seatId = seatButton.getActionCommand();
+        if (selectedSeats.contains(seatId)) {
+            selectedSeats.remove(seatId);
+            seatButton.setIcon(seatIcon); // Reset to default icon
+        } else {
+            selectedSeats.add(seatId);
+            seatButton.setIcon(null); // Visually indicate selection, change as needed
+        }
+    }
+
+    private void addNextButton() {
+        JButton nextButton = new JButton("Next");
+        nextButton.addActionListener(e -> {
+            if (!selectedSeats.isEmpty()) {
+                dispose();
+                new PaymentDetailsForm((JFrame) getParent(), selectedSeats).setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(this, "Please select a seat.", "Selection Required", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        JPanel nextButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        nextButtonPanel.add(nextButton);
+        getContentPane().add(nextButtonPanel, BorderLayout.SOUTH);
     }
 }
