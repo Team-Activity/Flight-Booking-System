f8fb5505d64f7a1906230c5dbd0fb357996f0459
Anton Joshua Dominguez <143872285+antonjoshuadominguez@users.noreply.github.com>
2023-12-20 20:22:10 +0800

    update

--- FlightBookingSystem/src/com/teamactivitiy/DBConnection.java
+++ FlightBookingSystem/src/com/teamactivitiy/DBConnection.java
@@ -3,7 +3,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:sqlite:flightbookingsystemdb.db";
+    private static final String URL = "jdbc:sqlite:FlightBookingSystem/flightbookingsystemdb.db";
+
+    static {
+        try {
+            // Explicitly load the SQLite JDBC driver
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
